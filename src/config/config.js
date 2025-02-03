60ccd2ac96f759621b534adaadef778998099bc0
DavidOrozco74 <david.orozco@smartsoft.com.co>
2025-02-03 05:04:03 -0500

    backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('MongoDB connected');
-    }catch(error){
+    } catch (error) {
         console.error(error.message);
     }
 };
